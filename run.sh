3c128d4ca26b84591077bc0e45e318902694e1ed
Sanghyun Park <sanghyun.park@kaist.ac.kr>
2025-03-26 00:49:27 +0900

    Modify run.sh to be connected with PWD

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --platform linux/amd64 --rm -it --cap-add=SYS_PTRACE --security-opt seccomp=unconfined pwndbg-x86 bash
+docker run --platform linux/amd64 --rm -it --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $PWD:/ctf pwndbg-x86 bash
